16357fc4c4ce25f8672bd6df4c3aad1e540db2a5
Chandra Prakash Jangid <chandra@sonohaler.com>
2025-07-22 17:50:25 +0530

    create controller

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
- import express from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -15,4 +15,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extends: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes 
+import userRouter from "./routes/user.routes.js";
+
+//router declaration
+app.use("/api/v1/users", userRouter)
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,23 +1,24 @@
-export { asyncHandler };
 
-
-const asyncHandler = () => {
-    (req, res, next) => {
-        Promise.resolve(func(req, res, next)).catch((error) =>
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) =>
             next(error)
         )
     }
 }
 
-const asyncHandlerNext = (func) => async (req, res, next) => {
-    try {
-        await func(req, res, next);
-    } catch (error) {
-        console.error("Error occurred in async handler:", error);
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message || "Internal Server Error",
-            error: error.stack || "No stack trace available"
-        })
-    }
-}
+// const asyncHandlerNext = (func) => async (req, res, next) => {
+//     try {
+//         return await func(req, res, next);
+//     } catch (error) {
+//         console.error("Error occurred in async handler:", error);
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message || "Internal Server Error",
+//             error: error.stack || "No stack trace available"
+//         })
+//     }
+// }
+
+
+export { asyncHandler };
